cf03a359ad8a7cda9f5124eef8a0fcfd5d27e803
MathioLucas <162763121+MathioLucas@users.noreply.github.com>
2025-03-27 16:22:51 -0400

    Create MainActivity.kt

---
+++ MainActivity.kt
@@ -0,0 +1,23 @@
+package com.example.travelplanner
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.travelplanner.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnLogin.setOnClickListener {
+            startActivity(Intent(this, LoginActivity::class.java))
+        }
+        binding.btnRegister.setOnClickListener {
+            startActivity(Intent(this, RegisterActivity::class.java))
+        }
+    }
+}
